c1bb49db4b78fdd764dd8f5c5a6fbb606a06c9bc
Joe Tran <tu.tran@ecaraid.com>
2024-07-01 17:39:06 +0700

    update list query account

--- src/main/java/com/mycompany/ketoan/repository/AccountRepository.java
+++ src/main/java/com/mycompany/ketoan/repository/AccountRepository.java
@@ -9,20 +9,13 @@ import java.util.Map;
 
 public class AccountRepository {
 	
-	private static final String LIST_ACCOUNT_QUERY = "SELECT \n" +
-			"case when ua.Cap = 'LEVEL1' then ua.Cap1 end as Cap1,\n" +
-			"case when ua.Cap = 'LEVEL2' then ua.Cap2 end as Cap2,\n" +
-			"case when ua.Cap = 'LEVEL3' then ua.Cap3 end as Cap3,\n" +
-			"ua.TenTK\n" +
-			"from (\n" +
-			"SELECT MaTaiKhoan as Cap1, null as Cap2, null as Cap3, TenTaiKhoan as TenTK, 'LEVEL1' as Cap FROM TaiKhoan\n" +
-			"union all\n" +
-			"SELECT tk1.MaTaiKhoan as Cap1, tk2.MaTaiKhoan as Cap2, null as Cap3, tk2.MaTieuKhoan as TenTK, 'LEVEL2' as Cap  FROM TieuKhoan tk2 join TaiKhoan tk1 on tk2.MaTaiKhoan = tk1.MaTaiKhoan \n" +
-			"union all\n" +
-			"SELECT tk2.MaTaiKhoan1 as Cap1, tk2.MaTaiKhoan2 as Cap2, tk3.MaTietKhoan as Cap3, tk3.TenTietKhoan as TenTK, 'LEVEL3' as Cap FROM TietKhoan tk3 join (SELECT tk1.MaTaiKhoan as MaTaiKhoan1 , tk2.MaTaiKhoan as MaTaiKhoan2\n" +
-			"FROM TieuKhoan tk2 join TaiKhoan tk1 on tk2.MaTaiKhoan = tk1.MaTaiKhoan) tk2\n" +
-			") ua where ('%%' is null or (ua.Cap1 like '%%'  or ua.Cap1 like '%%'  or  ua.Cap1 like '%%' )) \n" +
-			"ORDER BY ua.Cap1 asc, ua.Cap2 asc, ua.Cap3 asc";
+	private static final String LIST_ACCOUNT_QUERY = "SELECT case when ua.Cap = 'LEVEL1' then ua.Cap1 end as Cap1, case when ua.Cap = 'LEVEL2' then ua.Cap2 end as Cap2, case when ua.Cap = 'LEVEL3' then ua.Cap3 end as Cap3, ua.TenTK \n" +
+			"from (SELECT MaTaiKhoan as Cap1, null as Cap2, null as Cap3, TenTaiKhoan as TenTK, 'LEVEL1' as Cap\n" +
+			" FROM TaiKhoan union all SELECT tk1.MaTaiKhoan as Cap1, tk2.MaTieuKhoan as Cap2, null as Cap3, tk2.TenTieuKhoan as TenTK, 'LEVEL2' as Cap \n" +
+			" FROM TieuKhoan tk2 join TaiKhoan tk1 on tk2.MaTaiKhoan = tk1.MaTaiKhoan union all \n" +
+			" SELECT tk12.MaTaiKhoan as Cap1, tk12.MaTieuKhoan as Cap2, tk3.MaTietKhoan as Cap3, tk3.TenTietKhoan as TenTK, 'LEVEL3' as Cap FROM TietKhoan tk3\n" +
+			"LEFT JOIN (SELECT tk2.MaTieuKhoan, tk1.MaTaiKhoan FROM TieuKhoan tk2 join TaiKhoan tk1 ON tk2.MaTaiKhoan = tk1.MaTaiKhoan) tk12 ON tk12.MaTieuKhoan = tk3.MaTieuKhoan) ua\n" +
+			" where (:keyword is null or (ua.Cap1 like :keyword or ua.Cap1 like :keyword or ua.Cap1 like :keyword)) ORDER BY ua.Cap1 asc, ua.Cap2 asc, ua.Cap3 asc";
 	
 	public static List<AccountDTO> findAll(String keyword) {
 		ResultSet rs = QueryRepository.executeQuery(LIST_ACCOUNT_QUERY, Map.of("keyword", "%" + keyword + "%"));
